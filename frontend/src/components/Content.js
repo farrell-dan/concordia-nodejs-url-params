5a6ff1ec74a628a586706d6c548958c401c505fd
Dipti2021 <razdan.dipti@gmail.com>
2023-07-07 15:15:01 -0400

    Removed the FE and ex 6-8

--- frontend/src/components/Content.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Content = ({ children }) => <Wrapper>{children}</Wrapper>;
-
-const Wrapper = styled.div`
-  padding: 0 24px 24px;
-`;
-
-export default Content;
